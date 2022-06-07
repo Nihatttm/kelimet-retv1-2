22c040ca98e49c6c68aaa0635232a7674c3aa491
tankapf <107043192+tankapf@users.noreply.github.com>
2022-06-07 15:53:52 +0400

    Update yakalayıcı.py

--- kelime_bot/plugins/yakalayıcı.py
+++ kelime_bot/plugins/yakalayıcı.py
@@ -22,7 +22,7 @@ async def buldu(c:Client, m:Message):
     try:
         if m.chat.id in oyun:
             if m.text.lower() == oyun[m.chat.id]["kelime"]:
-                await c.send_message(m.chat.id,f"✨ Tebrikler !\n**{m.from_user.mention}** \n**<code>{oyun[m.chat.id]['kelime']}</code>** , Sözünü Buldu ✅")
+                await c.send_message(m.chat.id,f"✨ Təbriklər !\n**{m.from_user.mention}** \n**<code>{oyun[m.chat.id]['kelime']}</code>** , Sözünü Tapdı ✅")
                 if f"{m.from_user.mention}" in rating:
                     rating[f"{m.from_user.mention}"] += 1
                 else:
@@ -47,7 +47,7 @@ async def buldu(c:Client, m:Message):
                     for i in siralama:
                         siralama_text += i + "\n"
                     
-                    return await c.send_message(m.chat.id,f"✅ Oyun Bitti✓ \n\n📝 Puan :\n\n{siralama_text}\n\n Yeni Oyuna Başlamak İçin /game Yaza Bilirsiniz !")
+                    return await c.send_message(m.chat.id,f"✅ Oyun Bitdi✓ \n\n📝 Xal :\n\n{siralama_text}\n\n Yeni Oyuna Başlamaq üçün /basıa Yaza Bilərsiniz !")
                 
                 
                 
